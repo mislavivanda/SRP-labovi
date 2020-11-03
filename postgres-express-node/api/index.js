d88cc06ff7e627d4a35ef5238ded3857f1f546c4
Mislav Ivanda <mislavivanda454@gmail.com>
2020-11-03 14:39:46 +0100

    'Add_initial_login_logic'

--- postgres-express-node/api/index.js
+++ postgres-express-node/api/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const hello = require("./routes/hello");
 const user = require("./routes/user");
+<<<<<<< HEAD
 const medicalTest = require("./routes/medical-test");
 
 module.exports = () => {
@@ -10,5 +11,12 @@ module.exports = () => {
   user(router);
   medicalTest(router);
 
+=======
+const login = require("./routes/login");
+module.exports = () => {
+  hello(router);
+  user(router);
+  login(router);
+>>>>>>> 06c8afd... 'Add_initial_login_logic'
   return router;
 };
